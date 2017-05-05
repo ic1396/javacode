3db443642f1842dec47d9e69b88110a7705143d1
ic1396 <redsirius@foxmail.com>
2017-05-05 10:18:02 +0800

    Win7_Work_PC
    Change-Id: I3c6f1de11974025c6d991b3fa9d7c3e281e82a19
    Signed-off-by: ic1396 <redsirius@foxmail.com>

--- pro01/src/com/java/chap03/sec03/Person7.java
+++ pro01/src/com/java/chap03/sec03/Person7.java
@@ -5,14 +5,22 @@ class SanWei{
 	int w;//腰围
 	int h;//臀围
 }
+/**
+ * @author yush
+ * 参数值传递和引用传递的例子
+ */
 public class Person7 {
 	
+	/**
+	 * @param age        //int 值传递
+	 * @param sanWei     //类SanWei 引用传递 
+	 */
 	void speak(int age,SanWei sanWei){
 		System.out.println("我今年"+age+"岁。;");
 		System.out.println("我的三围是"+sanWei.b+","+sanWei.w+","+sanWei.h);
 		age=90;
 		System.out.println("方法里"+age+"。;");
-		sanWei.b=80;
+		sanWei.b=180;
 	}
 	public static void main(String[] args) {
 		Person7 zhangsan=new Person7();
@@ -21,8 +29,9 @@ public class Person7 {
 		sanWei.b=90;
 		sanWei.w=69;
 		sanWei.h=90;
-		zhangsan.speak(age,sanWei);
-		System.out.println(age);
-		System.out.println("调用地方，胸围"+sanWei.b);
+		zhangsan.speak(age,sanWei);  //变量age、sanWei作为参数执行zhangsan.speak()
+		System.out.println("----传递参数后--------");
+		System.out.println("方法外调用当前值，age="+age);
+		System.out.println("方法外调用当前值，sanWei.b="+sanWei.b);
 	}
 }
