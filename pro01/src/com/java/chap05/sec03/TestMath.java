053026814b3d26465f82eeb2136f83b477f135db
ic1396 <redsirius@foxmail.com>
2017-06-20 20:55:23 +0800

    Java常用类：Math

---
+++ pro01/src/com/java/chap05/sec03/TestMath.java
@@ -0,0 +1,16 @@
+package com.java.chap05.sec03;
+
+/**
+ * 
+ *
+ */
+public class TestMath {
+	
+	public static void main(String[] args) {
+		System.out.println("最大值：" + Math.max(1, 2));
+		System.out.println("最小值：" + Math.min(-1, 2));
+		System.out.println("四舍五入：" + Math.round(4.56));
+		System.out.println("3的4次方" + Math.pow(3, 4));
+	}
+
+}
